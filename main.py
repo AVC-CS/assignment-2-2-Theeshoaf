eae4a77f6297cc10f8309437760e7a7eed119cdc
jay <jayly.2506@gmail.com>
2023-10-23 09:00:42 -0700

    2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     celsius = int(input('Enter the celsius temperature:'))
     fahrenheit = 9/5 * celsius + 32 
 
-    print (f'The fahrenheit temperature is :\t{fahrenheit:.2f}')
+    print (f'The fahrenheit temperature is:\t{fahrenheit:.2f}')
 
     """
     ########################################
